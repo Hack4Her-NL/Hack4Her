214d79e0f0b1228a7f44b579981fffaf236d5d25
Joeyjdl <97511389+Joeyjdl@users.noreply.github.com>
2025-06-06 15:38:36 +0200

    Added room 5 and removed room names

--- src/routes/workshops/+page.svelte
+++ src/routes/workshops/+page.svelte
@@ -8,10 +8,15 @@
 
   // Room data
   const rooms = [
-    { name: "NU-4A25", capacity: "", size: "" },
-    { name: "NU-4B43", capacity: "", size: "" },
-    { name: "NU-4B47", capacity: "", size: "" },
-    { name: "NU-4B01", capacity: "", size: "" }
+    //{ name: "NU-4A25", capacity: "", size: "" },
+    //{ name: "NU-4B43", capacity: "", size: "" },
+    //{ name: "NU-4B47", capacity: "", size: "" },
+    //{ name: "NU-4B01", capacity: "", size: "" },
+    { name: "Room 1", capacity: "", size: "" },
+    { name: "Room 2", capacity: "", size: "" },
+    { name: "Room 3", capacity: "", size: "" },
+    { name: "Room 4", capacity: "", size: "" },
+    { name: "Room 5", capacity: "", size: "" }
   ];
 
   // Time slots
